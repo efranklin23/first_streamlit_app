a1ca24640fc978d08a1190d844ac49ac359497b3
efranklin23 <108199465+efranklin23@users.noreply.github.com>
2023-02-22 08:54:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('BreakfastMenu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale. Spinach, and Rocket Smoothie')
-streamlit.text('hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗Kale. Spinach, and Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
