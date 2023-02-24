37dccc4f496c00b9875c6845d96828f3b771df52
efranklin23 <108199465+efranklin23@users.noreply.github.com>
2023-02-24 08:49:07 -0600

    Update streamlit_app.py
    deleted json from screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # normalizes the data that is currently on the screen
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
